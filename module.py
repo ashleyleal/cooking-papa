fabd8adca2f251fe9bd1291a3019229182aaa1bf
chocoboash <anleal2004@gmail.com>
2022-04-30 22:05:14 -0400

    many changes

--- module.py
+++ module.py
@@ -1,5 +1,14 @@
+import pygame
+
 def draw_text(text, font, color, surface, x, y):
     textobj = font.render(text, 1, color)
     textrect = textobj.get_rect()
-    textrect.topleft = (x, y)
+    textrect.center = (x, y)
     surface.blit(textobj, textrect)
+
+def draw_image(image, scale, surface, x, y):
+    imagerect = image.get_rect()
+    imagescaled = pygame.transform.scale(image, (imagerect.right * scale, imagerect.bottom * scale))
+    imagerect.x, imagerect.y = image.get_width(), image.get_height()
+    surface.blit(imagescaled, imagescaled.get_rect(center = (x,y)))
+
