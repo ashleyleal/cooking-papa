73bcf02c244cc8cf6a260dae367a06d4c682a6d2
chocoboash <anleal2004@gmail.com>
2022-04-30 22:05:27 -0400

    many changes

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import pygame
 import math
 import time
 
-import module
-import button
+from module import draw_image, draw_text
+from button import Button
 
 # Initialize pygame library
 pygame.init()
@@ -23,10 +23,10 @@ GREEN = (167, 214, 118)
 NEXA_FONT = pygame.font.Font("assets/fonts/Nexa-Trial-Regular.ttf", 15)
 
 # Placeholder title text
-placeholder_title = pygame.image.load("assets/images/cookingPapaPlaceholderTitle.png")
+placeholder_title = pygame.image.load("assets/images/titles/cookingPapaPlaceholderTitle.png")
 
 # Placeholder button image
-placeholder_button = pygame.image.load("assets/images/buttonPlaceholder.png")
+placeholder_button = pygame.image.load("assets/images/buttons/buttonPlaceholder.png")
 
 # Sets up display window (w x h)
 size_x = 1280
@@ -39,41 +39,79 @@ pygame.display.set_caption("Cooking Papa")
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-# Variables to determine which state the game is in
-menu = True
-playing = False
-shopping = False
+# Universal return button
+return_button = Button(100, 50, placeholder_button, 0.5)
 
-# Initialize main menu buttons 
-
-start_button = button.Button((size_x / 2) - 80, (size_y / 2) - 50, placeholder_button, 0.8)
-customize_button = button.Button((size_x / 2) - 80, (size_y / 2) + 50, placeholder_button, 0.8)
-quit_button = button.Button((size_x / 2) - 80, (size_y / 2) + 150, placeholder_button, 0.8)
 
 def main_menu():
-    screen.fill(ORANGE) #screen fill for testing purposes only
-    screen.blit(pygame.transform.scale(placeholder_title, (450, 100)), ((size_x / 2) - 220, (size_y / 2) - 170))
+
+    # Make variables global so that they can be modified from within this function
+    global in_menu, running, is_shopping, is_playing
+
+    # Initialize main menu buttons 
+    start_button = Button((size_x / 2), (size_y / 2), placeholder_button, 0.9)
+    shop_button = Button((size_x / 2), (size_y / 2) + 100, placeholder_button, 0.9)
+    quit_button = Button((size_x / 2), (size_y / 2) + 200, placeholder_button, 0.9)
+
+    # Fill screen
+    screen.fill(ORANGE) 
+    
+    # Draw Game title image
+    draw_image(placeholder_title, 5, screen, size_x/2, 200)
+    
+    # Actions when start button pressed
     if start_button.draw(screen):
-        print("Start game")
-        menu = False
-        playing = True
-    if customize_button.draw(screen):
-        shopping = True
+        in_menu = False
+        is_playing = True
+    
+    # Actions when shop button pressed
+    if shop_button.draw(screen):
+        in_menu = False
+        is_shopping = True
+    
+    # Actions when quit button pressed
     if quit_button.draw(screen):
         global running
         running = False
 
+    pygame.display.update()
+
 def play():
-    screen.fill(ORANGE)
-    back = button.Button(50, 50, placeholder_button, 0.5)
-    module.draw_text("Welcome to Cooking Papa", NEXA_FONT, BLACK, screen, size_x/2, size_y/2)
+
+    # Make variables global so that they can be modified from within this function
+    global in_menu, running, is_shopping, is_playing
+    
+    screen.fill(BLUE)
+    draw_text("Game started", NEXA_FONT, BLACK, screen, size_x/2, size_y/2)
+
+    if return_button.draw(screen):
+        in_menu = True
+        is_playing = False
+
     pygame.display.update()
 
 def shop_menu():
-    print("Shop menu")    
+
+    # Make variables global so that they can be modified from within this function
+    global in_menu, running, is_shopping, is_playing
+    
+    screen.fill(LIGHT_ORANGE)
+    draw_text("Shop entered", NEXA_FONT, BLACK, screen, size_x/2, size_y/2)
+
+    if return_button.draw(screen):
+        
+        in_menu = True
+        is_shopping = False
+
+    pygame.display.update()  
 
 # Variable to determine whether main game loop is running
 running = True
+
+# Variables to determine which state the game is in
+in_menu = True
+is_playing = False
+is_shopping = False
 
 # Game loop
 while running:
@@ -83,25 +121,25 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        elif event.type == pygame.KEYDOWN:
-            print("button pressed")
+        # elif event.type == pygame.KEYDOWN:
+        #     print("button pressed")
 
-        elif event.type == pygame.KEYUP:
-            print("button released")
+        # elif event.type == pygame.KEYUP:
+        #     print("button released")
 
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("clicked")
+        # elif event.type == pygame.MOUSEBUTTONDOWN:
+        #     print("clicked")
 
-        elif event.type == pygame.MOUSEBUTTONUP:
-            print("click released")
+        # elif event.type == pygame.MOUSEBUTTONUP:
+        #     print("click released")
 
-    if menu:
+    if in_menu:
         main_menu()
 
-    if playing:
+    if is_playing:
         play()
 
-    if shopping:
+    if is_shopping:
         shop_menu()
     
     # Updates display
