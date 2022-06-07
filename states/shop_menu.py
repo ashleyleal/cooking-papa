2e86242c581905dc336a1874462b14743092a30f
Rubystone34 <fionna.e.cruz@gmail.com>
2022-06-07 15:51:53 -0400

    shop buttons

--- states/shop_menu.py
+++ states/shop_menu.py
@@ -16,6 +16,16 @@ class Shop_Menu(State):
         if actions["menu"]:
             main_menu = self.game.state_stack[0]
             main_menu.enter_state()
+        if actions["characters"]:
+          new_state = characters(self.game)
+          new_state.enter_state()
+        #if actions["colours"]:
+          #new_state = colours(self.game)
+          #new_state.enter_state()
+        if actions["music"]:
+          new_state = music(self.game)
+          new_state.enter_state()
+
 
     # Render loop that continously updates screen based on current conditions
     def render(self, surface):
@@ -31,26 +41,81 @@ class Shop_Menu(State):
         #(Draw buttons for confirm or decline purchase)
         #(Draw bg for confirm or decline purchase prompt)
   
-        self.shop_button_1 = Button((self.game.GAME_X / 2), (self.game.GAME_Y / 2) + 10, characters_button, 1)
-        self.shop_button_2 = Button((self.game.GAME_X / 2), (self.game.GAME_Y / 2) + 37, recolour_button, 1)
-        self.shop_button_3 = Button((self.game.GAME_X / 2), (self.game.GAME_Y / 2) + 64, music_button, 1)
+        self.characters_button = Button(80, 120, characters_button, 1)
+        self.recolour_button = Button((self.game.GAME_X / 2), 120, recolour_button, 1)
+        self.music_button = Button(242, 120, music_button, 1)
         
         #(Assign buttons for avaliable music, avaliable characters, avaliable colours, confirm and decline purchases)
       
         if return_button.draw(surface):
           self.game.actions ["menu"] = True
-        #if return_button.draw(surface):
-            #self.game.actions["menu"] = True
-          
-        #if characters_button.draw(surface):
-            #self.game.actions["characters"] = True
-          
-        #if recolour_button.draw(surface):
-            #self.game.actions["colours"] = True
-          
-        #if music_button.draw(surface):
-            #self.game.actions["music"] = True
+        if self.characters_button.draw(surface):
+            self.game.actions["characters"] = True
+        if self.recolour_button.draw(surface):
+          self.game.actions["colours"] = True
+        if self.music_button.draw(surface):
+          self.game.actions["music"] = True
 
+class characters(State):
+  # Inherit init method from State class 
+    def __init__(self, game):
+        State.__init__(self, game)
+    def update(self, actions):
+        super().update(actions)
+        if actions["shop"]:
+          new_state = Shop_Menu(self.game)
+          new_state.enter_state()
+     
+    def render(self, surface):
+      self.game.draw_image(menu_bg, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
+      self.game.draw_image(characters_button, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
+
+      if return_button.draw(surface):
+        self.game.actions["shop"] = True
+
+class colours(State):
+  # Inherit init method from State class 
+    def __init__(self, game):
+        State.__init__(self, game)
+    def update(self, actions):
+        super().update(actions)
+        if actions["shop"]:
+          new_state = Shop_Menu(self.game)
+          new_state.enter_state()
+
+    def render(self, surface):
+      self.game.draw_image(menu_bg, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
+      self.game.draw_image(recolour_button, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
+
+      if return_button.draw(surface):
+        self.game.actions["shop"] = True
+
+class music(State):
+  # Inherit init method from State class 
+    def __init__(self, game):
+        State.__init__(self, game)
+    def update(self, actions):
+        super().update(actions)
+        if actions["shop"]:
+          new_state = Shop_Menu(self.game)
+          new_state.enter_state()
+     
+    def render(self, surface):
+      self.game.draw_image(menu_bg, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
+      self.game.draw_image(music_button, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
+
+      if return_button.draw(surface):
+        self.game.actions["shop"] = True
+
+
+
+
+
+
+
+
+
+  
         #if self.game.actions["characters"] == True:
           #pass
           #draw all images for characters menu
@@ -67,12 +132,3 @@ class Shop_Menu(State):
         #draw all buttons for music menu 
           
           
-        """
-        if actions menu then
-          return to menu
-
-elif action actions charater
-      draw everything in the character menu
-
-      elif 
-        """
