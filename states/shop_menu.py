8c7cbf364b62877113be95e6bb43defbb6d10cf4
Rubystone34 <fionna.e.cruz@gmail.com>
2022-06-10 09:16:41 -0400

    commit

--- states/shop_menu.py
+++ states/shop_menu.py
@@ -56,10 +56,16 @@ class Shop_Menu(State):
         if self.music_button.draw(surface):
           self.game.actions["music"] = True
 
-class Characters(State):
+class characters(State):
   # Inherit init method from State class 
     def __init__(self, game):
         State.__init__(self, game)
+
+        self.prompt_confirm_purchase = False
+        self.owned_character_1 = False
+        self.owned_character_2 = False
+        self.owned_character_3 = False
+
     def update(self, actions):
         super().update(actions)
         if actions["shop"]:
@@ -69,11 +75,54 @@ class Characters(State):
     def render(self, surface):
       self.game.draw_image(menu_bg, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
       self.game.draw_image(characters_button, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
+      #self.game.draw_image(character_option_1)
+      #self.game.draw_image(character_option_2)
+      #self.game.draw_image(character_option_3)
+
+      #self.character_option_1 = Button()
+      #self.character_option_2 = Button()
+      #self.character_option_3 = Button()
 
       if return_button.draw(surface):
         self.game.actions["shop"] = True
+      #if character_option_1.draw(surface):
+        #self.prompt_confirm_purchase = True
+        
+        #self.game.draw_image(confirm_purchase)
+        #self.game.draw_image(confirm_purchase_button)
+        #self.game.draw_image(decline_purchase_button)
+        
+        #self.confirm_purchase_button = Button()
+        #self.decline_purchase_button = Button()
 
-class Colours(State):
+        #if confirm_purchase_button.draw(surface):
+          #if self.game.spend_gold == "insuficent funds":
+            #self.game.draw_image(insuficent_funds)
+            #self.game.draw_image(ok_button)
+
+            #self.ok_button = Button()
+
+            #if ok_button.draw(surface):
+              #self.game.actions["characters"] = True
+
+          #elif self.owned_character_1 = True:
+            #self.game.draw_image(character_owned)
+            #self.game.draw_image(ok_button)
+
+            #self.ok_button = Button()
+
+            #if ok_button(surface):
+              #self.game.actions["characters"] = True
+
+          #elif:
+            #self.game.spend_gold()
+            #self.character_owned = True
+
+          #if decline_purchase_button.draw(surface):
+            #self.game.actions["characters"] = True
+
+
+class colours(State):
   # Inherit init method from State class 
     def __init__(self, game):
         State.__init__(self, game)
@@ -86,11 +135,13 @@ class Colours(State):
     def render(self, surface):
       self.game.draw_image(menu_bg, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
       self.game.draw_image(recolour_button, 1, surface, self.game.GAME_X / 2, self.game.GAME_Y / 2)
+      #self.game.draw_image(recolour_option_1)
+      #self.game.draw_image(recolour_option_2)
 
       if return_button.draw(surface):
         self.game.actions["shop"] = True
 
-class Music(State):
+class music(State):
   # Inherit init method from State class 
     def __init__(self, game):
         State.__init__(self, game)
@@ -106,15 +157,12 @@ class Music(State):
 
       if return_button.draw(surface):
         self.game.actions["shop"] = True
-<<<<<<< HEAD
 
 
 
           
- """ 
- make a dictionary of all purchasable items and their prices (dictonary for each class for the items and prices in said class)
- when player clicks button that corresponds to purchasing the item, call the spend gold function, check return value 
- (if spend_gold = "insufficent funds" draw the image) 
- """     
-=======
->>>>>>> 1011077932c284de194678682096701a4dd54028
+ 
+ #make a dictionary of all purchasable items and their prices (dictonary for each class for the items and prices in said class)
+ #when player clicks button that corresponds to purchasing the item, call the spend gold function, check return value 
+ #(if spend_gold = "insufficent funds" draw the image) 
+     
